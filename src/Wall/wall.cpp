f30039ba750f7036964526561b699f304a62b58f
Mason Hawver <33504840+MasonJohnHawver42@users.noreply.github.com>
2019-06-23 12:07:48 -0500

    Update wall.cpp

--- src/Wall/wall.cpp
+++ src/Wall/wall.cpp
@@ -1,4 +1,5 @@
 #include "../grid/grid.cpp"
+#include <SFML/Graphics.hpp>
 
 //check
 
@@ -6,6 +7,7 @@ class Wall
 {
 private:
   Vector<int> * location;
+  sf::Texture texture;
 
 public:
   Wall() { location = new Vector<int>(0, 0); }
@@ -13,8 +15,23 @@ public:
   {
     location = loc;
   }
-
+  Wall(Vector<int> * loc, sf::Texture t)
+  {
+    location = loc;
+    texture = t;
+  }
   Vector<int> * getLocation();
+
+  void setTexture(sf::Texture t);
+  sf::Texture getTexture();
+
 };
 
 Vector<int> * Wall::getLocation() { return location; }
+
+void Wall::setTexture(sf::Texture t) {
+  texture = t;
+}
+sf::Texture Wall::getTexture() {
+  return texture;
+}
