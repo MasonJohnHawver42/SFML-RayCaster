bd43002367448c6eaf41a01f3e31a4607da5a677
Mason Hawver <33504840+MasonJohnHawver42@users.noreply.github.com>
2019-06-28 13:00:20 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,27 @@
-#include "Camera/camera.cpp"
+#include "MapEditor/mapEditor.cpp"
+#include "Actor/bullet.cpp"
+
 #include <ctime>
 
 int main(int argc, char const *argv[]) {
 
-  Grid<Wall> * map = new Grid<Wall>(8, 8);
+  Grid<Wall> * map = new Grid<Wall>(20, 20);
   World * world = new World(map);
 
+  Actor * barrel = new Actor(new Vector<double>(3.5, 3.5), 0);
+  Actor * barrel2 = new Actor(new Vector<double>(10.5, 10.5), 0);
+
+  sf::Texture barrelTexture;
+  barrelTexture.loadFromFile("../assets/ActorSprites/barrel.png");
+
+  barrel->setTexture(barrelTexture);
+  barrel2->setTexture(barrelTexture);
+
+  world->addActor(barrel);
+  world->addActor(barrel2);
+
   sf::Texture wallTexture;
-  wallTexture.loadFromFile("../assets/walls/brickWall.jpg");
+  wallTexture.loadFromFile("../assets/walls/stoneWall.jpg");
 
   for (int i = 0; i < world->getMap()->getRows(); i++)
   {
@@ -21,22 +35,32 @@ int main(int argc, char const *argv[]) {
     world->addWall(new Vector<int>(i, world->getMap()->getRows() - 1), wallTexture);
   }
 
-  wallTexture.loadFromFile("../assets/walls/wallaa.jpg");
-
-  world->addWall(new Vector<int>(2, 1), wallTexture);
-  world->addWall(new Vector<int>(1, 6), wallTexture);
-  world->addWall(new Vector<int>(5, 2), wallTexture);
-  world->addWall(new Vector<int>(5, 3), wallTexture);
-  world->addWall(new Vector<int>(4, 5), wallTexture);
-  world->addWall(new Vector<int>(5, 5), wallTexture);
-  world->addWall(new Vector<int>(6, 5), wallTexture);
+  //world->addWall(new Vector<int>(2, 1), wallTexture);
+  //world->addWall(new Vector<int>(1, 6), wallTexture);
+  //world->addWall(new Vector<int>(5, 2), wallTexture);
+  //world->addWall(new Vector<int>(5, 3), wallTexture);
+  //world->addWall(new Vector<int>(4, 5), wallTexture);
+  //world->addWall(new Vector<int>(5, 5), wallTexture);
+  //world->addWall(new Vector<int>(6, 5), wallTexture);
 
 
-  world->getMap()->toString();
+  //world->getMap()->toString();
 
-  Camera * cam = new Camera(world, new Vector<double>(1.5, 1.5), M_PI/2);
+  Camera * cam = new Camera(world, new Vector<double>(3.5, 3.5), M_PI/2);
 
-  sf::RenderWindow * window = new sf::RenderWindow(sf::VideoMode(800, 600), "TEST");
+  sf::RenderWindow * window = new sf::RenderWindow(sf::VideoMode(800, 600), "Ray Caster Engine V2");
+
+  MapEditor * editor = new MapEditor(world);
+
+  editor->edit(window);
+
+  Vector<double> * pos = new Vector<double>(-5, -5);
+
+  sf::Vector2i mp = sf::Mouse::getPosition(*window);
+
+  int origon = window->getSize().x / 2;
+
+  window->setMouseCursorVisible(false);
 
   while (window->isOpen())
     {
@@ -47,21 +71,38 @@ int main(int argc, char const *argv[]) {
             if (event.type == sf::Event::Closed) { window->close(); }
         }
 
-        time_t t = std::time(0);
-        long int before = static_cast<long int> (t);
+        window->clear(*new sf::Color(112, 112, 112));
 
-        window->clear();
+        sf::RectangleShape rect = *new sf::RectangleShape(*new sf::Vector2f(window->getSize().x, window->getSize().y / 2.0));
+        rect.setFillColor(*new sf::Color(26, 26, 26));
+        window->draw(rect);
 
         cam->display(800, window);
 
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) { cam->moveForward(.06); }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) { cam->moveBackward(.06); }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) { cam->turn(M_PI/-150); }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) { cam->turn(M_PI/150); }
+        double mag = .1;
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) { cam->moveForward(mag); }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) { cam->moveBackward(mag); }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) { cam->strafe(mag); }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) { cam->strafe(-1 * mag); }
+
+        mp = sf::Mouse::getPosition(*window);
+
+        double disToPP = (window->getSize().x / 2.0) / tan(cam->getFov() / 2);
+
+        cam->turn(atan((mp.x - origon) / disToPP));
+
+        origon = window->getSize().x / 2;
+
+        sf::Mouse::setPosition(*new sf::Vector2i(window->getSize().x / 2, window->getSize().y / 2), *window);
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) { window->close(); }
 
         window->display();
 
-        t = std::time(0);
-        long int after = static_cast<long int> (t);
+        for(int i = 0; i < world->numActors(); i++)
+        {
+          world->getActor(i)->act();
+        }
     }
 }
