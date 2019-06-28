ff88fdb8c0d925ce4d1336d42630feddbc81dc61
Mason Hawver <33504840+MasonJohnHawver42@users.noreply.github.com>
2019-06-28 12:58:58 -0500

    Update world.cpp

--- src/World/world.cpp
+++ src/World/world.cpp
@@ -1,18 +1,20 @@
-#include "../Actor/actor.cpp"
-#include <list>
+#include "../grid/grid.cpp"
+#include <vector>
 
+using namespace std;
+
+class Actor;
 
 class World {
 private:
   Grid<Wall> * map;
-  list<Actor> * actors;
+  std::vector<Actor*> actors;
 
 
 public:
   World()
   {
     map = new Grid<Wall>();
-    actors = new list<Actor>();
   }
 
   World(Grid<Wall> * m)
@@ -24,6 +26,13 @@ public:
   void addWall(Vector<int> *loc, sf::Texture t);
   void removeWall(Vector<int> loc);
 
+  void addActor(Actor * actor);
+  void removeActor(Actor * actor);
+
+  int numActors();
+
+  Actor * getActor(int index);
+
   Grid<Wall> * getMap();
 
 };
@@ -31,5 +40,18 @@ public:
 void World::addWall(Vector<int> * loc) { map->put(new Wall(loc), *loc); }
 void World::addWall(Vector<int> * loc, sf::Texture t) { map->put(new Wall(loc, t), *loc); }
 void World::removeWall(Vector<int> loc) { map->put(nullptr, loc); }
+
+void World::addActor(Actor * actor) { actors.push_back(actor); }
+void World::removeActor(Actor * actor) {
+  for (int i = 0; i < actors.size(); i++) {
+    if(actors[i] == actor) {
+      actors.erase(actors.begin() + i);
+    }
+  }
+}
+
+int World::numActors() { return actors.size(); }
+
+Actor * World::getActor(int index) { return actors[index]; }
 
 Grid<Wall> * World::getMap() { return map; }
