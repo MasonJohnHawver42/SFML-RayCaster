ecd9d75d368254306f6b8e3e8558d5fd6b8ec280
Mason Hawver <33504840+MasonJohnHawver42@users.noreply.github.com>
2019-06-28 12:57:20 -0500

    Update actor.cpp

--- src/Actor/actor.cpp
+++ src/Actor/actor.cpp
@@ -5,48 +5,92 @@ protected:
   Vector<double> * pos;
   Vector<double> * dir;
 
-  Grid<Wall> * grid;
+  double size; // radius
+
+  sf::Texture texture;
+
+  World * world;
 public:
   //dir is in rads
   Actor(Vector<double> * p, double dir)
   {
     pos = p;
     setDir(dir);
+
+    texture = *new sf::Texture();
+    sf::Image blank;
+    blank.create(1, 1, *new sf::Color(255, 255, 255));
+    texture.loadFromImage(blank);
+
+    size = 1;
   }
 
-  void setGrid(Grid<Wall> * g);
-  Grid<Wall> * getGrid();
+  void setWorld(World * w);
+  void removeSelfFromWorld();
 
-  Vector<double> * getLocation();
-  Vector<double> * getDirection();
+  World * getWorld();
 
-  void moveTo(Vector<double> * newPos);
+  Vector<double> * getPos();
+  Vector<double> * getDir();
+
+  double getSize();
+
+  sf::Texture getTexture();
+
+  void setTexture(sf::Texture t);
+
+  void moveTo(Vector<double> newPos);
   void move(Vector<double> offset);
 
   void moveForward(double mag);
   void moveBackward(double mag);
+
+  void strafe(double mag);
+
   void setDir(double rad);
 
   void turn(double rad);
 
-  void act();
+  virtual void act() {}
 
   Intersection * castRay(double rad);
 };
 
-void Actor::setGrid(Grid<Wall> * g) { grid = g;}
-Grid<Wall> * Actor::getGrid() { return grid; }
+void Actor::setWorld(World * w) { world = w; }
+void Actor::removeSelfFromWorld() {
+  if(world != nullptr)
+  {
+    world->removeActor(this);
+    world = nullptr;
+  }
+}
+
+World * Actor::getWorld() { return world; }
 
 
-Vector<double> * Actor::getLocation() { return pos; }
-Vector<double> * Actor::getDirection() { return dir; }
+Vector<double> * Actor::getPos() { return pos; }
+Vector<double> * Actor::getDir() { return dir; }
 
-void Actor::moveTo(Vector<double> * newPos) {
-  pos = newPos;
+double Actor::getSize() { return size; }
+
+sf::Texture Actor::getTexture() { return texture; }
+
+void Actor::setTexture(sf::Texture t) { texture = t; }
+
+void Actor::moveTo(Vector<double> newPos) {
+  double x, y;
+  x = newPos.getX();
+  y = newPos.getY();
+  if(1)//!world->getMap()->isOcupied(*new Vector<int>((int)x, (int)y)))
+  {
+    pos = new Vector<double>(x, y);
+  }
 }
 
 void Actor::move(Vector<double> offset) {
-  pos->add(offset);
+  Vector<double> p = *pos;
+  p.add(offset);
+  moveTo(p);
 }
 
 void Actor::moveForward(double mag) {
@@ -61,6 +105,13 @@ void Actor::moveBackward(double mag) {
   move(d);
 }
 
+void Actor::strafe(double mag) {
+  Vector<double> d = *dir;
+  d.perp();
+  d.mult(mag);
+  move(d);
+}
+
 void Actor::setDir(double rad) {
   dir = Vector<double>::radianToVector(rad);
 }
@@ -68,8 +119,6 @@ void Actor::setDir(double rad) {
 void Actor::turn(double rad) {
   dir->turn(rad);
 }
-
-void Actor::act() {}
 
 Intersection * Actor::castRay(double rad)
 {
@@ -83,6 +132,7 @@ Intersection * Actor::castRay(double rad)
   int yFlip;
   Vector<double> * rayYPos;
   Vector<double> * rayYInc;
+
 
   if (rayDir->getX() != 0)
   {
@@ -114,15 +164,15 @@ Intersection * Actor::castRay(double rad)
 
   int i = 0;
 
-  while (i < 10000)
+  while (i < 100)
   {
 
     if (rayDir->getX() != 0 && rayXPos->getDis(*pos) > rayYPos->getDis(*pos))
     {
       gridPos = new Vector<int>((int)(rayYPos->getX()) - ((yFlip - 1) / -2), (int)(rayYPos->getY()));
 
-      if(!grid->isValid(*gridPos)) { break; }
-      if(grid->isOcupied(*gridPos)) { return new Intersection(rayYPos, gridPos, rayYPos->getY() - floor(rayYPos->getY())); }
+      //if(!world->getMap()->isValid(*gridPos)) { break; }
+      if(world->getMap()->isOcupied(*gridPos)) { return new Intersection(rayYPos, gridPos, abs((rayYPos->getX() - gridPos->getX()) - (rayYPos->getY() - floor(rayYPos->getY())))); }
 
       rayYPos->add(*rayYInc);
     }
@@ -130,12 +180,16 @@ Intersection * Actor::castRay(double rad)
     else
     {
       gridPos = new Vector<int>((int) rayXPos->getX(), ((int) rayXPos->getY()) - ((xFlip - 1) / -2));
+      //if(!world->getMap()->isValid(*gridPos)) { break; }
+      Intersection * inter = new Intersection(rayXPos, gridPos, abs((1 - (rayXPos->getY() - gridPos->getY())) - (rayXPos->getX() - floor(rayXPos->getX()))));
 
-      if(!grid->isValid(*gridPos)) { break; }
-      if(grid->isOcupied(*gridPos)) { return new Intersection(rayXPos, gridPos, rayXPos->getX() - floor(rayXPos->getX())); }
-
+      if(world->getMap()->isOcupied(*gridPos)) {
+        return inter;
+      }
       rayXPos->add(*rayXInc);
     }
+
+    i++;
   }
 
   return new Intersection();
